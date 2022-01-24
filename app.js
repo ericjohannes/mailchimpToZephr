0f13cb6d16ceda7c52364dd10d43fdac5054d509
Eric Blom <eric.j.blom@gmail.com>
2022-01-24 11:58:19 -0600

    trying to make a test request with authorization

--- app.js
+++ app.js
@@ -1,22 +1,103 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const CryptoJS = require("crypto-js");
+
+const https = require('https')
+
+
+const makeHash = (path, body, queryString)=>{
+  const accessKey = process.env.zephrAccessKey
+  const secretKey = process.env.zephrSecretKey // "foobar"
+
+  // const accessKey = pm.variables.get('zephrAccessKey')
+  // const secretKey = pm.variables.get('zephrSecretKey')
+  // console.log(accessKey);
+  // console.log(secretKey);
+
+  // expanding variables in URL with pm.variables.replaceIn, then constructing path
+  // const path = `/${pm.variables.replaceIn(pm.request.url.path).join('/')}`
+  // console.log(pm.variables);
+  const timestamp = new Date().getTime().toString();
+  const nonce = (Math.random()).toString();
+
+  let hash = CryptoJS.algo.SHA256.create()
+  hash.update(secretKey)
+
+  // if (pm.request.body && Object.keys(pm.request.body).length) {
+  //     // expanding variables in body before updating hash with payload
+  //     hash.update(pm.variables.replaceIn(pm.request.body.raw))
+  // }
+  hash.update(body)
+  // console.log(pm.request.url.getQueryString());
+  hashString = hash.update(path)
+      .update(queryString)
+      .update(request.method)
+      .update(timestamp)
+      .update(nonce)
+      .finalize()
+      .toString()
+
+  const hmac = `ZEPHR-HMAC-SHA256 ${accessKey}:${timestamp}:${nonce}:${hashString}`.replace(/\r?\n|\r/, "");
+  return hmac
+}
+
+dotenv.config();
+
 const app = express()
 const port = 3000
+
+const baseUrl = "https://protocol.api.zephr.com/"
+const userPath = "/v3/users/"
+const emailUrl = "https://protocol.api.zephr.com/v3/users/?identifiers.email_address=clintperalta@gmail.com"
+const emailQueryBase = "identifiers.email_address="
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-app.post('/', (req, res) => {
-    console.log( req.body)
-    res.send('ok');
-});
+
 app.head('/', (req, res) => {
 
   // res.set('hello', 'world');
   console.log('head request')
 
 });
+app.post('/', (req, res) => {
+  // console.log( req.body)
+  // get email from req
+  console.log('email', req.body.email)
+  const subscriberEmail = req.body.email
+
+  // send email to zephr to get user id
+
+  // use user id to set all subscriptions to false
+
+  res.send('ok');
+});
+
 app.listen(port, () => {
+  const accessKey = process.env.zephrAccessKey
+  const secretKey = process.env.zephrSecretKey
+  console.log('accessKey', accessKey)
+  console.log('secretKey', secretKey)
+  const options = {
+    hostname: "https://protocol.api.zephr.com/",
+    path: "/v3/users/",
+    method: 'GET',
+    query: {
+      'identifiers.email_address': 'eric.j.blom@gmail.com'
+    },
+    headers: ''
+  }
+  
+  const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`)
+  
+    res.on('data', d => {
+      console.log(d)
+    })
+  })
+
   console.log(`Example app listening at http://localhost:${port}`)
 })
--- test_listener.py
+++ test_listener.py
@@ -3,27 +3,11 @@ import requests
 import json
 
 
-data = {
-  "type": "unsubscribe",
-  "fired_at": "2009-03-26 21:40:57",
-  "data": {
-    "action": "unsub",
-    "reason": "manual",
-    "id": "8a25ff1d98",
-    "list_id": "a6b5da1054",
-    "email": "api+unsub@mailchimp.com",
-    "email_type": "html",
-    "ip_opt": "10.20.10.30",
-    "campaign_id": "cb398d21d2",
-    "merges": {
-      "EMAIL": "api+unsub@mailchimp.com",
-      "FNAME": "Mailchimp",
-      "LNAME": "API",
-      "INTERESTS": "Group1,Group2"
-    }
-  }
-}
-# data = json.dumps(data)
+datafile = 'test.json'
+data = ''
+with open(datafile, 'r') as f:
+  data = json.load(f) 
+# data = json.dump(datafile)
 # url = 'http://127.0.0.1:3000'
 url = 'http://192.168.0.5:3000'
 response = requests.post(url, json=data)
