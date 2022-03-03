54837d2d98f4f8889e02794e0142322557bf89b0
Eric Blom <eric.j.blom@gmail.com>
2022-03-03 14:27:04 -0600

    port option on test listener

--- testApp.js
+++ testApp.js
@@ -1,10 +1,14 @@
 // having trouble knowing if mailchimp is connecting ok or if their is a bug in the app
 // run this to see if we get anything
+// runs with a command line argument --port for port to listen to
 
 const express = require('express')
 
+const parseArgs = require('minimist')
+const argv = parseArgs(process.argv.slice(2))
+
 const app = express();
-const port = 3000;
+const port = argv['port'];
 app.all("*", (req, res, next) => {
     console.log(req); // do anything you want here
     next();
