c3372e57716cbb40971a73c1dd8bc98fe6bf7952
Eric Blom <eric.j.blom@gmail.com>
2022-03-17 08:55:47 -0500

    certs in test app

--- testApp.js
+++ testApp.js
@@ -7,6 +7,19 @@ const express = require('express')
 const parseArgs = require('minimist')
 const argv = parseArgs(process.argv.slice(2))
 
+// get right certs on local machine or on ec2
+const ec2CertsPath = "/etc/certs/wildcard_protocol.com";
+let keyPath = 'certs/key.pem';
+let certPath = 'certs/cert.pem';
+if(fs.existsSync(ec2CertsPath)){
+    keyPath = `${ec2CertsPath}/star_protocol_com.key`;
+    certPath = `${ec2CertsPath}/star_protocol_com.crt`;
+}
+const options = {
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath)
+};
+
 const app = express();
 const port = argv['port'];
 app.all("*", (req, res, next) => {
@@ -15,7 +28,12 @@ app.all("*", (req, res, next) => {
     res.sendStatus(200)
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-
-})
+https
+  .createServer(options, app)
+  .listen(port, ()=>{
+    try {
+        console.log(`Listening at https://localhost:${port}`)
+    } catch (err) {
+        sendToSlack(err.stack)
+    }
+});
