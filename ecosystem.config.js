aaf62ead2f3ebe75852a530e2b245aba91e22060
Eric Blom <eric.j.blom@gmail.com>
2022-04-04 13:39:22 -0500

    add timestamps

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,7 @@ module.exports = {
     ignore_watch: ["data"],
     args: "--dev=false --port=443",
     error_file: ".logs/mailchimpToZephrErr.log",
-    out_file: ".logs/mailchimpToZephrOut.log"
+    out_file: ".logs/mailchimpToZephrOut.log",
+    log_date_format: "YYYY-MM-DD HH:mm Z"
   }]
 }
